3f4fb5d7492fcebde4d5f83425ec4b8c25b65d0c
Shreyas Sahoo <shreyas0502@gmail.com>
2026-03-20 20:16:46 +0530

    Add recursive function to check prime numbers

---
+++ RecursionPrime.c
@@ -0,0 +1,23 @@
+// To check if a number is Prime using recursion 
+
+#include <stdio.h>
+int isPrime(int n, int i) {
+    if (n % i == 0)
+        return 0;
+    if (i * i > n)
+        return 1;
+
+    return isPrime(n, i + 1);
+}
+int main() 
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    if (isPrime(num, 2))
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+    return 0;
+}
